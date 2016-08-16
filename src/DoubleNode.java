c4e3a82658f3c5bbfa2e8031ac6b52d17db07268
EdgaRamirez <ram15236@uvg.edu.gt>
2016-08-15 21:37:02 -0600

    V 0.2 Nodos Interface lista Clase pila abstracta

---
+++ src/DoubleNode.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+
+/**
+ * @author Pareja 9 Hugo Elvira 15249 y Edgar Ramírez 15236 
+ * @version 11/08/2016
+ */
+
+public class DoubleNode <E>{
+	protected E data; 
+	protected DoubleNode<E> nextElement; 
+	protected DoubleNode<E> previousElement;
+	
+	/**
+	 * @param v
+	 * @param next
+	 * @param previous
+	 */
+	public DoubleNode(E v, DoubleNode<E> next, DoubleNode<E> previous) { 
+		data = v; 
+		nextElement = next; 
+		previousElement = previous;
+		if (nextElement != null){ 
+			nextElement.previousElement = this; 
+			previousElement = previous;
+		}
+		if (previousElement != null) {
+			previousElement.nextElement = this;
+		}
+	}
+	/**
+	 * @param v
+	 */
+	public DoubleNode(E v){  
+		// post: constructs a single element 
+		this(v,null,null);
+	}
+	/**
+	 * @return
+	 */
+	public DoubleNode<E> next() {
+		// TODO Auto-generated method stub
+		return nextElement;
+	}
+	/**
+	 * @return
+	 */
+	public E value() {
+		// TODO Auto-generated method stub
+		return data;
+	}
+	/**
+	 * @return
+	 */
+	public DoubleNode<E> previous() {
+		// TODO Auto-generated method stub
+		return previousElement;
+	}
+	
+	/**
+	 * @param next
+	 */
+	public void setNext(DoubleNode<E> next) {
+	// post: sets reference to new next value  
+		nextElement = next; 
+	}
+
+}
+
